dcfcdaa0a1a39cd58cd584a4c85e0ea5a499295a
BaldingBoi <duong.nguyen@finhay.com.vn>
2023-11-15 16:06:01 +0700

    cpi

--- components/pages/macro-data/CPI.tsx
+++ components/pages/macro-data/CPI.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+import { Bot, ChevronRight, HelpCircle } from "lucide-react";
 import {
 	Chart as ChartJS,
 	CategoryScale,
 	LinearScale,
 	BarElement,
 	Title,
-	Tooltip,
+	Tooltip as ChartTooltip,
 	Legend,
 } from "chart.js";
 import { Chart } from "react-chartjs-2";
@@ -16,102 +16,181 @@ ChartJS.register(
 	LinearScale,
 	BarElement,
 	Title,
-	Tooltip,
+	ChartTooltip,
 	Legend
 );
-import CPIData from "@/data/cpi.json";
+import CPIQuarterly from "@/data/cpi-quarterly.json";
+import CPIAnnual from "@/data/cpi-annual.json";
+
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import resolveConfig from "tailwindcss/resolveConfig";
 import tailwindConfig from "@/tailwind.config.js";
 
 const styleConfig = resolveConfig(tailwindConfig);
 
+const chartOptions = {
+	responsive: true,
+	plugins: {
+		legend: {
+			display: true,
+			position: "bottom",
+			reverse: true,
+		},
+		title: {
+			display: false,
+		},
+	},
+	scales: {
+		x: {
+			grid: {
+				display: false,
+			},
+			ticks: {
+				autoSkip: true,
+				maxTicksLimit: 6,
+			},
+		},
+		y: {
+			display: true,
+			grid: {
+				display: false,
+			},
+			min: 80,
+		},
+		y1: {
+			display: true,
+			position: "right",
+			grid: {
+				display: false,
+			},
+		},
+	},
+};
+
 const CPI = () => {
 	return (
-		<div className="flex flex-col gap-2">
+		<Tabs defaultValue="quarterly" className="flex flex-col gap-2">
 			<div className="w-full flex items-center justify-between">
 				<div className="font-semibold text-2xl border-l-4 border-primary pl-2">
-					Consumer Price Index (CPI)
+					<div className="flex items-center gap-2">
+						<div>Consumer Price Index (CPI)</div>
+						<TooltipProvider>
+							<Tooltip delayDuration={0}>
+								<TooltipTrigger>
+									<HelpCircle className="text-yellow-500 rounded-full" />
+								</TooltipTrigger>
+								<TooltipContent>
+									<div className="text-slate-600 w-[250px]">
+										The index which aims to measure the
+										average price change in the purchases of
+										goods and services by households
+										nationwide
+									</div>
+								</TooltipContent>
+							</Tooltip>
+						</TooltipProvider>
+					</div>
 				</div>
-				<Button variant={"link"} className="text-sm flex items-center">
-					<div>See Details</div>
-					<ChevronRight />
-				</Button>
+				<div className="flex items-center gap-4">
+					<Button
+						variant={"ghost"}
+						className="flex items-center gap-2"
+					>
+						<Bot />
+						<div>Analyze with AI</div>
+					</Button>
+					<TabsList>
+						<TabsTrigger value="quarterly">Quarterly</TabsTrigger>
+						<TabsTrigger value="annual">Annual</TabsTrigger>
+					</TabsList>
+				</div>
 			</div>
-			<div className="w-full text-slate-600">
-				The index which aims to measure the average price change in the
-				purchases of goods and services by households nationwide
-			</div>
-			<div className="w-full p-4">
-				<Chart
-					type="line"
-					options={{
-						responsive: true,
-						plugins: {
-							legend: {
-								display: true,
-								position: "bottom",
-								reverse: true,
-							},
-							title: {
-								display: false,
-							},
-						},
-						scales: {
-							x: {
-								grid: {
-									display: false,
+			<TabsContent value="quarterly">
+				<div className="w-full p-4">
+					<Chart
+						type="bar"
+						//@ts-ignore
+						options={chartOptions}
+						data={{
+							labels: CPIQuarterly["Time"].slice(-12),
+							datasets: [
+								{
+									label: "Change from the same time period of previous year (%)",
+									data: CPIQuarterly["Change"].slice(-12),
+									type: "line",
+									fill: false,
+									borderColor: "#16A34A",
+									backgroundColor: "#16A34A",
+									tension: 0.4,
+									pointBorderWidth: 0,
+									pointBackgroundColor: "transparent",
+									yAxisID: "y1",
+									borderWidth: 2,
 								},
-								ticks: {
-									autoSkip: true,
-									maxTicksLimit: 6,
+								{
+									label: "Consumer Price Index",
+									data: CPIQuarterly["CPI"].slice(-12),
+									borderWidth: 0,
+									backgroundColor:
+										//@ts-ignore
+										styleConfig.theme?.colors?.slate[300],
+									borderColor: "transparent",
+									type: "bar",
+									yAxisID: "y",
 								},
-							},
-							y: {
-								display: true,
-								grid: {
-									display: false,
+							],
+						}}
+					/>
+					;
+				</div>
+			</TabsContent>
+			<TabsContent value="annual">
+				<div className="w-full p-4">
+					<Chart
+						type="bar"
+						//@ts-ignore
+						options={chartOptions}
+						data={{
+							labels: CPIAnnual["Time"].slice(-6),
+							datasets: [
+								{
+									label: "Change from the same time period of previous year (%)",
+									data: CPIAnnual["Change"].slice(-6),
+									type: "line",
+									fill: false,
+									borderColor: "#16A34A",
+									backgroundColor: "#16A34A",
+									tension: 0.4,
+									pointBorderWidth: 0,
+									pointBackgroundColor: "transparent",
+									yAxisID: "y1",
+									borderWidth: 2,
 								},
-								min: 95,
-							},
-							y1: {
-								display: true,
-								position: "right",
-								grid: {
-									display: false,
+								{
+									label: "Consumer Price Index",
+									data: CPIAnnual["CPI"].slice(-6),
+									borderWidth: 0,
+									backgroundColor:
+										//@ts-ignore
+										styleConfig.theme?.colors?.slate[300],
+									borderColor: "transparent",
+									type: "bar",
+									yAxisID: "y",
 								},
-							},
-						},
-					}}
-					data={{
-						labels: CPIData["Time"].slice(-36),
-						datasets: [
-							{
-								label: "Change from the same time period of previous year (%)",
-								data: CPIData["Change"].slice(-36),
-								type: "line",
-								fill: false,
-								borderColor: "black",
-								tension: 0.4,
-								pointBorderWidth: 0,
-								pointBackgroundColor: "transparent",
-								yAxisID: "y1",
-								borderWidth: 2,
-							},
-							{
-								label: "Consumer Price Index",
-								data: CPIData["CPI"].slice(-36),
-								borderWidth: 2,
-								backgroundColor: "transparent",
-								borderColor: "#16A34A",
-								type: "bar",
-								yAxisID: "y",
-							},
-						],
-					}}
-				/>
-			</div>
-		</div>
+							],
+						}}
+					/>
+					;
+				</div>
+			</TabsContent>
+		</Tabs>
 	);
 };
 
--- components/pages/macro-data/ExchangeRate.tsx
+++ components/pages/macro-data/ExchangeRate.tsx
@@ -25,11 +25,6 @@ import {
 	TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "@/tailwind.config.js";
-
-const styleConfig = resolveConfig(tailwindConfig);
-
 ChartJS.register(
 	CategoryScale,
 	LinearScale,
@@ -70,92 +65,86 @@ const chartOptions = {
 
 const ExchangeRate = () => {
 	return (
-		<div className="flex flex-col gap-2">
-			<Tabs defaultValue="quarterly">
-				<div className="w-full flex items-center justify-between">
-					<div className="font-semibold text-2xl border-l-4 border-primary pl-2">
-						<div className="flex items-center gap-2">
-							<div>Exchange rates (USD/JPY)</div>
-							<TooltipProvider>
-								<Tooltip delayDuration={0}>
-									<TooltipTrigger>
-										<HelpCircle className="text-yellow-500 rounded-full" />
-									</TooltipTrigger>
-									<TooltipContent>
-										<div className="w-full text-slate-600">
-											Tokyo market interbank rates (U.S.
-											dollar/Yen) spot as of 5:00
-											PM(JST)／end of month
-										</div>
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-						</div>
-					</div>
-					<div className="flex items-center gap-4">
-						<Button
-							variant={"ghost"}
-							className="flex items-center gap-2"
-						>
-							<Bot />
-							<div>Analyze with AI</div>
-						</Button>
-						<TabsList>
-							<TabsTrigger value="quarterly">
-								Quarterly
-							</TabsTrigger>
-							<TabsTrigger value="annual">Annual</TabsTrigger>
-						</TabsList>
+		<Tabs defaultValue="quarterly" className="flex flex-col gap-2">
+			<div className="w-full flex items-center justify-between">
+				<div className="font-semibold text-2xl border-l-4 border-primary pl-2">
+					<div className="flex items-center gap-2">
+						<div>Exchange rates (USD/JPY)</div>
+						<TooltipProvider>
+							<Tooltip delayDuration={0}>
+								<TooltipTrigger>
+									<HelpCircle className="text-yellow-500 rounded-full" />
+								</TooltipTrigger>
+								<TooltipContent>
+									<div className="w-[250px] text-slate-600">
+										Tokyo market interbank rates (U.S.
+										dollar/Yen) spot as of 5:00 PM(JST)／end
+										of month
+									</div>
+								</TooltipContent>
+							</Tooltip>
+						</TooltipProvider>
 					</div>
 				</div>
-				<TabsContent value="quarterly">
-					<div className="w-full p-4">
-						<Line
-							data={{
-								labels: RateDataQuarterly["Time"].slice(-12),
-								datasets: [
-									{
-										label: "USD/JPY",
-										data: RateDataQuarterly["Rate"].slice(
-											-12
-										),
-										fill: false,
-										borderColor: "#16A34A", //primary
-										borderWidth: 2,
-										tension: 0.4,
-										pointBorderWidth: 0,
-										pointBackgroundColor: "transparent",
-									},
-								],
-							}}
-							options={chartOptions}
-						/>
-					</div>
-				</TabsContent>
-				<TabsContent value="annual">
-					<div className="w-full p-4">
-						<Line
-							data={{
-								labels: RateDataAnnual["Time"].slice(-6),
-								datasets: [
-									{
-										label: "USD/JPY",
-										data: RateDataAnnual["Rate"].slice(-6),
-										fill: false,
-										borderColor: "#16A34A", //primary
-										borderWidth: 2,
-										tension: 0.4,
-										pointBorderWidth: 0,
-										pointBackgroundColor: "transparent",
-									},
-								],
-							}}
-							options={chartOptions}
-						/>
-					</div>
-				</TabsContent>
-			</Tabs>
-		</div>
+				<div className="flex items-center gap-4">
+					<Button
+						variant={"ghost"}
+						className="flex items-center gap-2"
+					>
+						<Bot />
+						<div>Analyze with AI</div>
+					</Button>
+					<TabsList>
+						<TabsTrigger value="quarterly">Quarterly</TabsTrigger>
+						<TabsTrigger value="annual">Annual</TabsTrigger>
+					</TabsList>
+				</div>
+			</div>
+			<TabsContent value="quarterly">
+				<div className="w-full p-4">
+					<Line
+						data={{
+							labels: RateDataQuarterly["Time"].slice(-12),
+							datasets: [
+								{
+									label: "USD/JPY",
+									data: RateDataQuarterly["Rate"].slice(-12),
+									fill: false,
+									borderColor: "#16A34A", //primary
+									borderWidth: 2,
+									tension: 0.4,
+									pointBorderWidth: 0,
+									pointBackgroundColor: "transparent",
+								},
+							],
+						}}
+						options={chartOptions}
+					/>
+				</div>
+			</TabsContent>
+			<TabsContent value="annual">
+				<div className="w-full p-4">
+					<Line
+						data={{
+							labels: RateDataAnnual["Time"].slice(-6),
+							datasets: [
+								{
+									label: "USD/JPY",
+									data: RateDataAnnual["Rate"].slice(-6),
+									fill: false,
+									borderColor: "#16A34A", //primary
+									borderWidth: 2,
+									tension: 0.4,
+									pointBorderWidth: 0,
+									pointBackgroundColor: "transparent",
+								},
+							],
+						}}
+						options={chartOptions}
+					/>
+				</div>
+			</TabsContent>
+		</Tabs>
 	);
 };
 
