82dbd248cea8c9cfd933bd976d0840bf16faaf14
BaldingBoi <duong.nguyen@finhay.com.vn>
2023-11-16 16:58:13 +0700

    ...

--- query/common.ts
+++ query/common.ts
@@ -1,15 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { SupportedToken } from "@/lib/types";
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+// import { SupportedToken } from "@/lib/types";
 
-export const useSupportedToken = (chainId: number | undefined) =>
-	useQuery({
-		queryKey: ["txnHistory"],
-		queryFn: async () => {
-			if (!chainId) return;
-			const { data } = await axios.get(
-				`${process.env.NEXT_PUBLIC_API}/common/token/${chainId}`
-			);
-			return data as SupportedToken[];
-		},
-	});
+// export const useSupportedToken = (chainId: number | undefined) =>
+// 	useQuery({
+// 		queryKey: ["txnHistory"],
+// 		queryFn: async () => {
+// 			if (!chainId) return;
+// 			const { data } = await axios.get(
+// 				`${process.env.NEXT_PUBLIC_API}/common/token/${chainId}`
+// 			);
+// 			return data as SupportedToken[];
+// 		},
+// 	});
